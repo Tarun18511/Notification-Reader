b9ef7f70fd39bdda5711e7d6980458b6ae167cb6
Tarunvir <any.email@gmail.com>
2021-02-14 18:25:59 +0530

    Given more clarity to the code

--- app/src/main/java/messages/gohool/messages/MainActivity.java
+++ app/src/main/java/messages/gohool/messages/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-//    TextView messageTextView;
     Button showButton;
     private SharedPreferences pref;
     public static final String SHARED_PREFERENCE2 = "SharedPreferences";
@@ -25,40 +24,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         showButton = (Button) findViewById(R.id.showButton);
-//        messageTextView = (TextView) findViewById(R.id.messageTextView);
         allMessagesET = (EditText) findViewById(R.id.allMessageET);
         showButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 pref = getSharedPreferences(SHARED_PREFERENCE2, 0);
-//                SharedPreferences sharedPreferences = PreferenceManager
-//                        .getDefaultSharedPreferences(MainActivity.this);
                 String name = pref.getString(KEY, "default value");
-
-//                MyAccessibilityService myservice = new MyAccessibilityService();
-//                if(myservice.pref.contains("Messages"))
-//                String text = myservice.getSharedPreferences();
-//                messageTextView.setText(name);
                 allMessagesET.setText(name);
                 SharedPreferences.Editor editor = pref.edit();
                 editor.putString(KEY, "");
                 editor.commit();
             }
         });
-    }
-
-    public void pMesaage(Bundle extras) {
-
-        printMessage(extras);
-    }
-
-    public void printMessage(Bundle mExtras) {
-
-//        TextView mssTextView = (TextView) findViewById(R.id.messageTextView);
-        String senderName = mExtras.getString("android.title");
-        String message = mExtras.getString("android.text");
-//        mssTextView.setText(senderName + ": " + message + "\n");
     }
 
 }
--- app/src/main/java/messages/gohool/messages/MyAccessibilityService.java
+++ app/src/main/java/messages/gohool/messages/MyAccessibilityService.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 public class MyAccessibilityService extends AccessibilityService {
 
-//    private static final CharSequence[] NULL = {};
     public SharedPreferences pref;
     public static final String SHARED_PREFERENCES = "SharedPreferences";
     public static final String KEY = "Messages";
@@ -39,78 +38,26 @@ public class MyAccessibilityService extends AccessibilityService {
 
         if(event.getPackageName().toString().equals("com.whatsapp")) {
 
-            Parcelable parcelable = event.getParcelableData();
-            Notification notif = (Notification) parcelable;
             pref = getSharedPreferences(SHARED_PREFERENCES, 0);
             SharedPreferences.Editor editor = pref.edit();
             Notification notification = (Notification) event.getParcelableData();
-            CharSequence[] lines;
-            lines = notification.extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
-
-
+            CharSequence[] lines = notification.extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
             int i = 0;
             String message = " ";
             String senderName = " ";
-//
-//            String previousData = " ";
-////            if(lines == NULL) {
-//                for(CharSequence msg : lines) {
-////                senderName = " ";
-//                    Log.d("Line " + i, (String) msg);
-//                    message += (String) " " + msg;
-//                    i += 1;
-//                }
-//
-//            }
-//            else {
-//                senderName = extras.getString("android.title");
-//                message = extras.getString("android.text");
-//                previousData = pref.getString(KEY, "nothing is here");
-//            }
-
-
-
-//            senderName = extras.getString("android.title");
-//            message = extras.getString("android.text");
             String previousData = pref.getString(KEY, "nothing is here");
             if(lines != null) { // To remove null pointer error giving any sequence of text
                 for (CharSequence msg : lines) {
-                    //                senderName = " ";
                     Log.d("Line " + i, (String) msg);
                     message += (String) "\n" + msg;
                     i += 1;
                 }
             }
-
-            int flag = 0;
-//            if(flag != 0) {
-//                previousData = pref.getString(KEY, "nothing is here");
-//            }
-
             if(senderName != "WhatsApp")
             editor.putString(KEY, previousData + "\n" + senderName + ": " + message + "\n");
             editor.commit();
-            flag++;
-//            MainActivity mActivity = new MainActivity();
-//            mActivity.pMesaage(extras);
-
-//            String senderName = extras.getString("android.title");
-//            String message = extras.getString("android.text");
-//            SharedPreferences sharedPreferences = PreferenceManager
-//                    .getDefaultSharedPreferences(this);
-//            SharedPreferences.Editor editor = sharedPreferences.edit();
-//            editor.putString("Messages", senderName + ": " + message + "\n");
-//            editor.apply();
-
-//            Log.d("text: ", getSharedPreferences());
-//            getSharedPreferences();
         }
 
-    }
-
-    public String getSharedPreferences() {
-        pref = getSharedPreferences(SHARED_PREFERENCES, 0);
-        return pref.getString(KEY, "nothing is here");
     }
 
 
